a915ae0d0ad740bae5f7c23ec9e7f74e82ad4a50
Rusty_Joe_Stalin <kylecherewyk@hotmail.com>
2017-03-26 00:56:10 +0000

    gave up on css stuff - too much work for now

--- app/admin/about_page.rb
+++ app/admin/about_page.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register AboutPage do
+  permit_params :about_text , :contact_info
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
