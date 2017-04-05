787ad63c802e53f47c5fe6c574d98055539b826a
Rusty_Joe_Stalin <kylecherewyk@hotmail.com>
2017-04-05 20:23:51 +0000

    fixed remove for shopping cart

--- app/controllers/showcases_controller.rb
+++ app/controllers/showcases_controller.rb
@@ -1,6 +1,9 @@
+# require 'set'
 class ShowcasesController < ApplicationController
+  before_action :initliaze_session
+
   def index
-    @products = Product.all
+    @products = Product.all.order(name: :asc)
   end
 
   def about_pages
@@ -20,7 +23,7 @@ class ShowcasesController < ApplicationController
     @category_params = params[:category]
 
     if @category_params.present?
-        @result = Product.where("name LIKE ? AND category_id = ?", '%'+@search_params+'%',  @category_params)
+        @result = Product.where("name LIKE ? AND category_id = ?", '%'+@search_params+'%',  @category_params).order(name: :asc)
         # @result = Category.find(@category_params)
     elsif @search_params == nil
         @result = Category.find(@category_params).products
@@ -30,4 +33,32 @@ class ShowcasesController < ApplicationController
     #  @result = Category.find(params[:id]).products
 
   end
+
+  def remeber_cart
+     session[:items] << params[:id].to_i
+     redirect_to :back
+   end
+
+   def remeber_remove
+    session[:items].delete(params[:id].to_i)
+    redirect_to :back
+  end
+
+  def cart
+
+    @items_to_purchase = Product.find(session[:items])
+
+  end
+
+  private
+
+  def initliaze_session
+      session[:items]||=[]
+      # @items_to_purchase|| = nil
+    end
+    #
+    # def increment_visit_count
+    #   session[:visitCount]+=1
+    #   @visitCount = session[:visitCount]
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,22 @@ Rails.application.routes.draw do
   get 'showcases/about_pages', to:'showcases#about_pages', as: 'about'
 
   get 'showcases/results/', to:'showcases#results' , as: 'results'
+
+  get 'showcases/cart', to: 'showcases#cart' , as: 'cart'
   # removed the id from results.
+  post 'showcases/checkout' =>'showcases#checkout', as: 'checkout'
+
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources 'showcases', only:[:index,:show,:category,:about]
+   resources :showcases , only:[:index,:show] do
+     member do
+       post :remeber_cart #Post /customers/remember
+       post :remeber_remove
+     end
+
+   end
 
   root to: 'showcases#index'
 end
